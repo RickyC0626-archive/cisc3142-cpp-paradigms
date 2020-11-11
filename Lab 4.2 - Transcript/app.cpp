e495d7f3aa80501a5b406de24aad60284188ee54
Ricky Chon <rickychon99@gmail.com>
2020-11-11 16:43:45 -0500

    Fix indentation

--- Lab 4.2 - Transcript/app.cpp
+++ Lab 4.2 - Transcript/app.cpp
@@ -14,36 +14,36 @@ bool compare(const Student &s1, const Student &s2);
 
 int main()
 {
-	try
+    try
     {
-		vector<Student> students;
+        vector<Student> students;
 
-		load("students.data", students);
+        load("students.data", students);
 
-		sort(students.begin(), students.end(), compare);
+        sort(students.begin(), students.end(), compare);
 
-		write(students);
-		cout << students.size() << " records processed";
+        write(students);
+        cout << students.size() << " records processed";
 
-		return 0;
-	} 
+        return 0;
+    } 
     catch(string message)
     {
-		cout << "*** Exception *** " << message << endl;
-		exit(1);
-	}
+        cout << "*** Exception *** " << message << endl;
+        exit(1);
+    }
 }
 
 void load(string fname, vector<Student> &students)
 {
-	ifstream ifs(fname.c_str());
-	if (!ifs) throw string("input file " + fname + " not found");
+    ifstream ifs(fname.c_str());
+    if (!ifs) throw string("input file " + fname + " not found");
 
     int id, course_id, credits;
     string name;
     char grade;
 
-	while (!ifs.eof() && ifs >> id >> name)
+    while (!ifs.eof() && ifs >> id >> name)
     {
         Student s(id, name);
         
@@ -52,19 +52,19 @@ void load(string fname, vector<Student> &students)
             ifs >> credits >> grade;
             s.getCourses().push_back(Course(course_id, credits, grade));
         }
-		students.push_back(s);
-	}
-	ifs.close();
+        students.push_back(s);
+    }
+    ifs.close();
 }
 
 bool compare(const Student &s1, const Student &s2)
 {
-	return s1.getGPA() > s2.getGPA();
+    return s1.getGPA() > s2.getGPA();
 }
 
 void write(const vector<Student> &students)
 {
-	for (auto student : students)
-		cout << student;
+    for (auto student : students)
+        cout << student;
     cout << endl;
 }
